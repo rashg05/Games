847c92a8eb5eb7ac635dd5b2e8304ad666cd1e5c
rashg05 <gajra.rashmi0504@gmail.com>
2022-01-24 12:47:24 +0530

    Added: first position of player

---
+++ SinglePlayerGameUC1.java
@@ -0,0 +1,11 @@
+package com.bridgelab.snakeandladder;
+
+public class SinglePlayerGameUC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int points = 0;
+		System.out.println("So, lets start the game with position " + points);
+	}
+
+}
