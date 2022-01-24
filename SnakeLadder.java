51d966f53cb131172e056275f29e7f88bf1c3f01
rashg05 <gajra.rashmi0504@gmail.com>
2022-01-24 12:42:47 +0530

    Added: welcome messege for game

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelab.snakeandladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Snake And Ladder world");
+	}
+
+}
